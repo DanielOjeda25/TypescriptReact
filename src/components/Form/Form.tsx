7aef5dd737eb56b8c325f8619f19a13279bcc702
Daniel Alejandro Ojeda <ojedadanielalejandro333@gmail.com>
2022-07-14 21:43:40 -0300

    formulario terminado

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,25 +1,36 @@
-import { useState } from 'react';
-
-interface FormState {
-  
+import useNewSubForm from '../../assets/hook/useNewSubs';
+import { Sub } from '../../types'
+interface FormProps {
+  onNewSub: (newSub: Sub) => void
 }
 
-const Form = () => {
-  const [inputValue, setInputValue] = useState({
-    nick: '',
-    subMonths: 0,
-    avatar: '',
-    description: ''
-  })
+const Form = ({ onNewSub }: FormProps) => {
+  /* const [inputValue, setInputValue] = useState<FormState['inputValue']>(INITIAL_STATE) */
 
-  const handleSubmit = () => {
+  /**
+   * *El use reducer necesita un reducer, como primer argumento y un estado inicial como segundo argumento
+   */
+  const [inputValue, dispatch] = useNewSubForm()
 
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    onNewSub(inputValue)
+    handleClick()
   }
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-    setInputValue({
-      ...inputValue,
-      [e.target.name]: e.target.value
+    const { name, value } = e.target
+    dispatch({
+      type: 'change_value',
+      payload: {
+        inputName: name,
+        inputValue: value
+      }
+    })
+  }
+  const handleClick = () => {
+    dispatch({
+      type: 'clear'
     })
   }
   return (
@@ -30,6 +41,7 @@ const Form = () => {
         <input onChange={handleChange} value={inputValue.subMonths} type="number" name="subMonths" placeholder="Subscription Months" />
         <textarea onChange={handleChange} value={inputValue.description} name="description" placeholder="Description"></textarea>
         <button type="submit">Save new Sub!</button>
+        <button type="button" onClick={handleClick}>Clear the form</button>
       </form>
     </div>
   )
